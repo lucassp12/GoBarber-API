28a167bc2f8dbb3099271a75019a68a6af136a8d
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-08-28 20:54:37 -0300

    Criação de loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hellow Rockeatseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Santos',
+    email: 'lucas@lucas',
+    password_hash: '123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
