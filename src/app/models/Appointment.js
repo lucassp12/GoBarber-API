99ca6d33836d385c7581e507b13dcb59ab616d13
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-09-18 21:02:00 -0300

    Agendamento de serviço

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,7 +1,7 @@
 import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
 
-class User extends Model {
+class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
@@ -25,4 +25,4 @@ class User extends Model {
   }
 }
 
-export default User;
+export default Appointment;
