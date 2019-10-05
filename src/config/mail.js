8123fbfc85621ea7b2802f00995aee9d95b0d901
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-10-04 21:50:30 -0300

    Variaveis Ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'af2bd3925d6c7d',
-    pass: 'c37ab922287f4b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://538060e4d3274578b7dbed0164e1ce27@sentry.io/1768745',
+  dsn: process.env.SENTRY_DSN,
 };
